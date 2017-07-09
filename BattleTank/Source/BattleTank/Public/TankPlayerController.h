398f2d4b2ff52e70acac6e145ce275786cf34727
DRAGONBORN\Beast Killer <saaranshvatsa@yahoo.in>
2017-07-09 18:42:11 +0530

    Working UI

---
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/PlayerController.h"
+#include "TankPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK_API ATankPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
