0813ea0b4e83aef13b787a800ee33da1a73eb389
DRAGONBORN\Beast Killer <saaranshvatsa@yahoo.in>
2017-07-21 17:17:59 +0530

    Cursor Improvements

--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -34,7 +34,7 @@ public:
 		void Fire();
 
 protected:
-
+	UPROPERTY(BlueprintReadOnly)
 	UTankAimingComponent* TankAimingComponent = nullptr;
 
 private:
--- BattleTank/Source/BattleTank/Public/TankPlayerController.h
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -15,9 +15,11 @@ class BATTLETANK_API ATankPlayerController : public APlayerController
 {
 	GENERATED_BODY()
 	
-private:
-	ATank* GetControlledTank() const;
 
+protected:
+	UFUNCTION(BlueprintCallable, Category = Setup)
+	ATank* GetControlledTank() const;
+private:
 	virtual void BeginPlay() override;
 
 	virtual void Tick(float DeltaTime) override;
